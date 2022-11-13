7435f652f690c25a41779071454b3c13556d7061
BenjaminWills <benjaminwills047@gmail.com>
2022-11-13 20:30:10 +0000

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# portfolio-webiste
-Just having fun and learning how to make a website
+# My Portfolio Website
+
+Just having fun and learning how to make a website using **react**.
--- app.js
+++
@@ -1,7 +0,0 @@
-const num1 = document.getElementById("firstnum");
-const num2 = document.getElementById("secondnum");
-
-function adder() {
-  sum = num1 + num2;
-  document.write(String(sum));
-}
