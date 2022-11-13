09e8f665d667631f06362240614969958013cca5
BenjaminWills <benjaminwills047@gmail.com>
2022-11-13 23:04:43 +0000

    Added Todo component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div>
-        <h2>TITLE</h2>
-        <div>
-          <button>Delete</button>
-        </div>
-      </div>
+      <Todo />
     </div>
   );
 }
---
+++ my-app/src/components/Todo.js
@@ -0,0 +1,14 @@
+// Adding a Todo component
+
+function Todo() {
+  return (
+    <div className="card">
+      <h2>TITLE</h2>
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
